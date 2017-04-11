34d6b08434a85e5516a1ea89e9084f896360d718
Your Name <you@example.com>
2017-04-12 01:10:14 +0600

    assignment 07

--- smartForm.js
+++ smartForm.js
@@ -1,3 +1,40 @@
 // Your jQuery goes here
 
-var userData = {};
+(function(smartForm, $) {
+
+    smartForm.init = function() {
+        smartForm.analytics();
+    };
+
+    smartForm.analytics = function() {
+        var $smartForm = $('.js-smart-form');
+
+        if (!$smartForm.length) {
+            return;
+        }
+
+        var $smartFormInputs = $smartForm.find('input, select');
+        if (!$smartFormInputs.length) {
+            return;
+        }
+
+        var currentStep = $('.js-smartform-step').val();
+        var activityName = 'SmartForm_Step' + currentStep + 'Interacted';
+
+        $smartForm.find('input, select').change(function() {
+            // Uses the js-cookie plugin.
+            if (Cookies.get(activityName) === undefined) {
+                Cookies.set(activityName, 'true');
+                $.ajax({
+                    type: 'GET',
+                    url: '/api/Analytics/LogCustomActivity',
+                    data: {
+                        ActivityType: activityName
+                    }
+                });
+            }
+        });
+
+    };
+
+}(_.smartForm = _.smartForm || {}, jQuery));
